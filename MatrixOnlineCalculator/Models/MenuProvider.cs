ac73a9d2fb2eebf1aebd7366c717b230bdcc524d
vihmp <vihmp.chel@mail.ru>
2021-07-20 11:31:02 +0500

    added menu generation

---
+++ MatrixOnlineCalculator/Models/MenuProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace MatrixOnlineCalculator.Models
+{
+    public class MenuProvider
+    {
+        private static Dictionary<(string Controller, string Action), string> calculators =
+            new Dictionary<(string Controller, string Action), string>()
+            {
+                [("MatrixCalculator", "CalculateDeterminant")] = "Вычисление определителя матрицы"
+            };
+
+        public static Dictionary<(string Controller, string Action), string> GetCalculators()
+        {
+            return calculators;
+        }
+
+        public static string GetCalculatorName(string controller, string action)
+        {
+            return calculators[(controller, action)];
+        }
+    }
+}
