9dbeb5da70001e4ddb7fe9dbdf0bbb599a4b9947
vihmp <vihmp174@gmail.com>
2022-08-28 20:17:55 +0500

    add unit tests for MathHtmlHelper

---
+++ UnitTests/TestUtils/IHtmlContentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Html;
+using System.IO;
+using System.Text.Encodings.Web;
+
+namespace UnitTests.TestUtils
+{
+    public static class IHtmlContentExtensions
+    {
+        public static string AsString(this IHtmlContent content)
+        {
+            using (var writer = new StringWriter())
+            {
+                content.WriteTo(writer, HtmlEncoder.Default);
+
+                return writer.ToString();
+            }
+        }
+    }
+}
